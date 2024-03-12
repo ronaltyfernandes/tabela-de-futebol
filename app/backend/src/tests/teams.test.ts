aa632e638fb6a95a99e533291ea9c70019e797cf
ronalty fernandes <ronaltyfernandess@gmail.com>
2024-03-12 19:27:18 -0300

    teste teams find all

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -5,6 +5,10 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
+import Teams from '../database/models/TeamsModel';
+import {mapStatusHTTP, message} from '../utils/mapStatusHttp'
+
+import { allTeamsMock } from './mocks/mockTeams'
 
 import { Response } from 'superagent';
 
@@ -12,7 +16,20 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Testando a rota teams', () => {
+  beforeEach(function () { sinon.restore(); });
+
+  it('testando retorno do findAll para a lista de produtos', async () => {
+    // const mockFindOneReturn = Product.build(mockProductsList);
+
+    sinon.stub(Teams, 'findAll').resolves(allTeamsMock as any);
+    
+    const httpReponse = await chai.request(app).get('/teams').send(allTeamsMock);    
+
+    expect(httpReponse.status).to.equal(mapStatusHTTP.successful);
+    expect(httpReponse.body).to.deep.equal(allTeamsMock);
+
+  });
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -39,7 +56,4 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(true).to.be.eq(true);
-  });
 });
